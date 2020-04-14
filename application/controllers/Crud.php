158b860107b3a85ad9943acee8025535908449b3
luqmansen <luqmansen@gmail.com>
2020-04-14 12:03:07 +0700

    show all data view

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -8,20 +8,43 @@ class Crud extends CI_Controller
 		parent::__construct();
 		$this->load->model('area');
 		$this->load->model('photo');
+		$this->load->model('category');
 		$this->load->helper('url');
 		$this->load->library('upload');
 
 	}
 
-	function index(){
-		echo "hello";
+	function index()
+	{
+		$model = array('area', 'photo', 'category');
+		$data = array();
+		foreach ($model as $m) {
+			$data[$m] = $this->$m->get_all();
+		}
+		foreach ($data["photo"] as $i) {
+			$area = $this->area->get_row_by_id($i->area_id);
+			$i->area_id = $area->area_name;
+		}
+
+		foreach ($data["area"] as $i) {
+			$area = $this->category->get_row_by_id($i->id_category);
+			$i->id_category = $area->category_name;
+		}
+
+		$this->load->view('management/v_show_data', $data);
 	}
 
-	function new_geo(){
-		$this->load->view('management/v_upload_geo');
+	function new_geo()
+	{
+		$category = $this->category->get_all();
+		$data = array('category' => $category);
+		$this->load->view('management/v_upload_geo', $data);
 	}
-	function new_photo(){
-		$this->load->view('management/v_upload_photo');
+	function new_photo()
+	{
+		$area = $this->area->get_all();
+		$data = array('area' => $area);
+		$this->load->view('management/v_upload_photo', $data);
 	}
 
 	function add_geo(){
@@ -30,8 +53,8 @@ class Crud extends CI_Controller
 		$geojson_data  = file_get_contents($location);
 
 		$data = array(
-			'area_name'  => $this->input->post('area_name'),
-			'id_category' => $this->input->post('area_code'),
+			'area_name' => $this->input->post('area_name'),
+			'id_category' => $this->input->post('id_category'),
 			'geojson_data' => $geojson_data,
 		);
 
@@ -41,7 +64,6 @@ class Crud extends CI_Controller
 	}
 
 	function add_photo(){
-
 		$location = $_FILES['photo_data']['tmp_name'];
 		$photo_data  = file_get_contents($location);
 
---
+++ application/views/management/v_show_data.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta charset="utf-8">
+	<title>Data Barang</title>
+	<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+	<link href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css" rel="stylesheet">
+	<style>
+		.ppcont {
+			width: 200px;
+		}
+
+		a {
+			text-decoration: none;
+			display: inline-block;
+			padding: 8px 16px;
+		}
+
+		a:hover {
+			background-color: #ddd;
+			color: black;
+		}
+
+		.previous {
+			background-color: #f1f1f1;
+			color: black;
+		}
+	</style>
+</head>
+<body>
+<a href="<?= base_url() ?>" class="previous">&laquo; Back</a>
+<div class="container">
+	<h1>Data <small>Area </small></h1>
+	<table class="table table-bordered table-striped" id="area">
+		<thead>
+		<tr>
+			<td>id</td>
+			<td>Nama Area</td>
+			<td>Deskripsi Area</td>
+			<td>Jenis Kategori</td>
+		</tr>
+		</thead>
+		<tbody>
+		<?php
+		foreach ($area as $a):
+			?>
+			<tr>
+				<td><?php echo $a->id; ?> </td>
+				<td><?php echo $a->area_name; ?> </td>
+				<td><?php echo $a->area_description; ?> </td>
+				<td><?php echo $a->id_category; ?> </td>
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
+
+	<h1>Data <small>Category </small></h1>
+	<table class="table table-bordered table-striped" id="category">
+		<thead>
+		<tr>
+			<td>id</td>
+			<td>Nama Kategori</td>
+			<td>Warna</td>
+		</tr>
+		</thead>
+		<tbody>
+		<?php
+		foreach ($category as $a):
+			?>
+			<tr>
+				<td><?php echo $a->id; ?> </td>
+				<td><?php echo $a->category_name; ?> </td>
+				<td bgcolor=<?php echo $a->color; ?>></td>
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
+
+	<h1>Data <small>Photo</small></h1>
+	<table class="table table-bordered table-striped" id="photo">
+		<thead>
+		<tr>
+			<td>id</td>
+			<td>Nama Area</td>
+			<td>Photo</td>
+		</tr>
+		</thead>
+		<tbody>
+		<?php
+		foreach ($photo as $a):
+			?>
+			<tr>
+				<td><?php echo $a->id; ?> </td>
+				<td><?php echo $a->area_id; ?> </td>
+				<td><img class="ppcont" src="data:image/png;base64,<?php echo $a->photo_data; ?>"></td>
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
+</div>
+
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+
+<script>
+	$(document).ready(function () {
+		$('.table').DataTable();
+	});
+</script>
+</body>
+</html>
